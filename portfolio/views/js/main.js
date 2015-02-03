c2fd2d06fb34257bb07878a23bad02890f59243b
pnellesen <pnellesen@hotmail.com>
2015-02-02 18:40:45 -0600

    going back to old resize code

--- portfolio/views/js/main.js
+++ portfolio/views/js/main.js
@@ -450,7 +450,7 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-	
+	/*
 	var pizzaNodes = document.getElementsByClassName('randomPizzaContainer');// Won't work in older browsers.
 	var pizzaNodesLen = pizzaNodes.length;
 	for (var i = 0; i < pizzaNodesLen; i++) {
@@ -458,13 +458,14 @@ var resizePizzas = function(size) {
 	      var newwidth = (pizzaNodes[i].offsetWidth + dx) + 'px';
 	      pizzaNodes[i].style.width = newwidth;
 	}
-	/* Replaced the following with the function above. Used getElementsByClassName instead of querySelectorAll
+	*/
+	/* Replaced the following with the function above. Used getElementsByClassName instead of querySelectorAll */
 	 for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
-    */
+    
   }
 
   changePizzaSizes(size);
