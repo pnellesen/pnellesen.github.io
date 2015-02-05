a409ea7e63398d008f93d8cfc255fa26839363e3
Pat Nellesen <pnellesen@hotmail.com>
2015-02-05 17:40:15 -0600

    Update main.js

--- portfolio/views/js/main.js
+++ portfolio/views/js/main.js
@@ -516,8 +516,8 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
 var items = document.querySelectorAll('.mover');
-var numberOfItems = items.length;
-  for (var i = 0; i < numberOfItems; i++) {
+console.log("number of items: " + items.length);
+  for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
